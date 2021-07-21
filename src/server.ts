f8611019a50d3347dbccd8a86dd6186f68e7c8f6
zd247 <zduy2407@gmail.com>
2021-07-21 17:50:43 +0700

    initial commit from me, add some comments

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,9 @@ import Logger from './core/Logger';
 import { port } from './config';
 import app from './app';
 
+// starts the express server browser
 app
   .listen(port, () => {
     Logger.info(`server running on port : ${port}`);
   })
-  .on('error', (e) => Logger.error(e));
+  .on('error', (e: any) => Logger.error(e));
